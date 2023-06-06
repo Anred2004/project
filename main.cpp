347a8420585a33319f2106c4a710f226b27d1d35
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-06 16:58:41 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <iostream>
+#include "Logger.h"
+#include "Game.h"
+
+int main()
+{
+    Logger& logger = Logger::getInstance();
+    try {
+        // Создание окна
+        sf::RenderWindow window(sf::VideoMode(800, 600), "Game");
+
+        // Создание объекта игры
+        std::unique_ptr<Game> game(new Game());
+
+        // Основной цикл программы
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) {
+                    window.close();
+                }
+            }
+
+            // Обновление игры
+            game->update();
+
+            // Отрисовка игры
+            window.clear(sf::Color::White);
+            game->draw(window);
+            window.display();
+        }
+    }
+    catch (const std::exception& ex) 
+    {
+        // Логирование исключения
+        Logger::getInstance().logError(ex.what());
+        throw;
+    }
+
+    return EXIT_SUCCESS;
+}
