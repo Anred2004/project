9b1249eeda38d1782a13345a0010c1052db643ad
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-08 02:48:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,28 @@
+// main.cpp
 #include <SFML/Graphics.hpp>
-#include <memory>
 #include <iostream>
 #include "Logger.h"
 #include "Game.h"
 
 int main()
 {
-    Logger& logger = Logger::getInstance();
+    Logger logger;
     try {
         // Создание окна
         sf::RenderWindow window(sf::VideoMode(800, 600), "Game");
-
         // Создание объекта игры
         std::unique_ptr<Game> game(new Game());
-
         // Основной цикл программы
         while (window.isOpen()) {
             sf::Event event;
             while (window.pollEvent(event)) {
-                if (event.type == sf::Event::Closed) {
+                if (event.type == sf::Event::Closed) 
+                {
                     window.close();
                 }
             }
-
             // Обновление игры
             game->update();
-
             // Отрисовка игры
             window.clear(sf::Color::White);
             game->draw(window);
@@ -35,7 +32,7 @@ int main()
     catch (const std::exception& ex) 
     {
         // Логирование исключения
-        Logger::getInstance().logError(ex.what());
+        logger.logError(ex.what());
         throw;
     }
 
