5f17d1303f6639fd9501de378d109397a0e0f984
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-08 02:46:34 +0200

    Update Ball.cpp

--- Ball.cpp
+++ Ball.cpp
@@ -1,38 +1,56 @@
+// Ball.cpp
 #include "Ball.h"
 #include "Floor.h"
 
-Ball::Ball() : m_shape(10.f), m_rate(200.f, -200.f)
+Ball::Ball(): m_shape(10.f), m_rate(5.f, -5.f) // переменные содержащие форму круга и его скорость
 {
-    m_shape.setFillColor(sf::Color::Red);
-    m_shape.setPosition(400.f, 300.f);
-}
 
-void Ball::update(const std::vector<std::unique_ptr<GameObject>>& objects)
+    m_shape.setFillColor(sf::Color::Red);
+    float yOset = rand() % 100; // Случайный сдвиг от 0 до 99
+    m_shape.setPosition(400.f, 280.f+yOset);
+
+}
+void Ball::update(const std::vector<GameObject*>& objects)
 {
     // Обновление позиции мяча
     float dt = 1.f / 60.f;
     m_shape.move(m_rate * dt);
+    m_gameOver = false;
 
-    //111// Проверка столкновения с ракетками
-    for (const auto& object : objects) {
-        auto floor = dynamic_cast<Floor*>(object.get());
-        if (floor) {
-            if (m_shape.getGlobalBounds().intersects(floor->getShape().getGlobalBounds())) {
+    for (GameObject* object : objects) 
+    {
+        Floor* floor = dynamic_cast<Floor*>(object);
+        if (floor) 
+        {
+            sf::FloatRect ballBounds = m_shape.getGlobalBounds();
+            sf::FloatRect floorBounds = floor->getShape().getGlobalBounds();
+            if (ballBounds.left < floorBounds.left + floorBounds.width
+                && ballBounds.left + ballBounds.width > floorBounds.left
+                && ballBounds.top < floorBounds.top + floorBounds.height
+                && ballBounds.top + ballBounds.height > floorBounds.top) {
+
                 m_rate.y = -m_rate.y;
             }
         }
     }
-
-    // Проверка столкновения с границами окна
-    if (m_shape.getPosition().x < 0.f || m_shape.getPosition().x > 800.f) {
+    if (m_shape.getPosition().x < 0.f || m_shape.getPosition().x > 800.f) 
+    {
         m_rate.x = -m_rate.x;
     }
-    if (m_shape.getPosition().y < 0.f) {
-        m_rate.y = -m_rate.y;
+   //ДОДЕЛАТЬ!
+    if (m_shape.getPosition().y < 0.f || m_shape.getPosition().y > 600.f)
+    {
+       // Останавливаем скорость мяча       
+      m_rate = {0.f, 0.f};          
+      m_gameOver = true;
+
     }
 }
 
 void Ball::draw(sf::RenderTarget& target) const
 {
-    target.draw(m_shape);
+    if (m_gameOver == false)
+    {
+        target.draw(m_shape);
+    }
 }
