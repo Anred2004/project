51a5f9c56dcf06b974a38cd9acd8a43e62160031
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-08 02:44:19 +0200

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -1,16 +1,17 @@
+// Ball.h
 #pragma once
-
 #include "GameObject.h"
 
 class Ball : public GameObject
 {
 public:
     Ball();
-
-    void update(const std::vector<std::unique_ptr<GameObject>>& objects) override;
+    void update(const std::vector<GameObject*>& objects) override;
     void draw(sf::RenderTarget& target) const override;
-    // Ball specific drawing logic  
+    
 private:
     sf::CircleShape m_shape;
     sf::Vector2f m_rate;
+    bool m_gameOver;
+
 };
