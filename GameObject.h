2deb713fcecb86ccc110d8bce9f3500b0e44a89a
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-08 02:45:40 +0200

    Update GameObject.h

--- GameObject.h
+++ GameObject.h
@@ -1,15 +1,14 @@
+// GameObject.h базовый класс
 #pragma once
 
 #include <SFML/Graphics.hpp>
 #include <vector>
-#include <memory>
 
 class GameObject
 {
 public:
    
-    virtual void update(const std::vector<std::unique_ptr<GameObject>>& objects) = 0;
+    virtual void update(const std::vector<GameObject*>& objects) = 0;
     virtual void draw(sf::RenderTarget& target) const = 0;
-
-    virtual ~GameObject() = default;
+    virtual GameObject::~GameObject() {}
 };
