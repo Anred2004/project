9055aef7dbacda5484b895de18e75dd2272f459f
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-08 02:44:43 +0200

    Update Floor.h

--- Floor.h
+++ Floor.h
@@ -1,5 +1,5 @@
+// Floor.h
 #pragma once
-
 #include "GameObject.h"
 
 class Floor : public GameObject
@@ -7,12 +7,17 @@ class Floor : public GameObject
 public:
     Floor(float x, float y);
 
-    void update(const std::vector<std::unique_ptr<GameObject>>& objects) override;
+    void update(const std::vector<GameObject*>& objects) override;
     void draw(sf::RenderTarget& target) const override;
-
+    void setPlayer(int player) 
+    { 
+        m_player = player; 
+    }
     sf::RectangleShape& getShape();
 
 private:
     sf::RectangleShape m_shape;
     float m_speed;
+    int m_player;
+    
 };
