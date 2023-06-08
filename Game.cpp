1d70a385ced90482cb5cdd0864fc630ce5c0a2d2
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-08 21:40:11 +0200

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -5,10 +5,10 @@
 Game::Game()
 {   // Создание мяча и ракеток и игроков
     m_objects.push_back(new Ball());
-    m_objects.push_back(new Floor(100.f, 500.f));
-    m_objects.push_back(new Floor(100.f, 100.f));
-    static_cast<Floor*>(m_objects[1])->setPlayer(1);
-    static_cast<Floor*>(m_objects[2])->setPlayer(2);
+    m_objects.push_back(new Floor(100.f, 510.f));
+    m_objects.push_back(new Floor(100.f, 60.f));
+    static_cast<Floor*>(m_objects[1]) -> setPlayer(1); // мы приводим указатель на базовый класс к указателю на производный класс 
+    static_cast<Floor*>(m_objects[2]) -> setPlayer(2); // Floor, чтобы вызвать функцию setPlayer, которая определена только для класса Floor.
 }
 Game::~Game() {
     // Удаляем все объекты
@@ -25,11 +25,11 @@ void Game::update()
         object->update(m_objects);
     }
 }
-void Game::draw(sf::RenderTarget& target)
+void Game::draw(sf::RenderWindow& window)
 {
     // Отрисовка объектов
     for (GameObject* object : m_objects)
     {
-        object->draw(target);
+        object->draw(window);
     }
 }
