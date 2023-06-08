f725c1a9fa68a84f79d2200226ac5e6ecdc0b485
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-08 21:38:20 +0200

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -6,12 +6,12 @@ class Ball : public GameObject
 {
 public:
     Ball();
-    void update(const std::vector<GameObject*>& objects) override;
-    void draw(sf::RenderTarget& target) const override;
+    void update(const std::vector<GameObject*>& objects) override; // метод обновляет состояние объекта на основе переданного ему списка объектов
+    void draw(sf::RenderWindow& window) const override; // метод отрисовки
     
 private:
-    sf::CircleShape m_shape;
-    sf::Vector2f m_rate;
-    bool m_gameOver;
+    sf::CircleShape m_shape; // переменная формы круга
+    sf::Vector2f m_rate;//переменна содержит информацию о скорости и направлении движения обьекта
+    
 
 };
