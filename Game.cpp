470569ad0f20bf9ea545198c31ef84c7c37fc6c2
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-08 02:47:31 +0200

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,28 +1,34 @@
+// Game.cpp
 #include "Game.h"
 #include "Ball.h"
 #include "Floor.h"
-
 Game::Game()
-{
-    // Создание мяча и ракеток
-    m_objects.push_back(std::make_unique<Ball>());
-    m_objects.push_back(std::make_unique<Floor>(100.f, 500.f));
-    m_objects.push_back(std::make_unique<Floor>(600.f, 500.f));
+{   // Создание мяча и ракеток и игроков
+    m_objects.push_back(new Ball());
+    m_objects.push_back(new Floor(100.f, 500.f));
+    m_objects.push_back(new Floor(100.f, 100.f));
+    static_cast<Floor*>(m_objects[1])->setPlayer(1);
+    static_cast<Floor*>(m_objects[2])->setPlayer(2);
 }
-
+Game::~Game() {
+    // Удаляем все объекты
+    for (GameObject* object : m_objects) 
+    {
+        delete object;
+    }
+}
 void Game::update()
 {
     // Обновление объектов
-    for (auto& object : m_objects) 
+    for (GameObject* object : m_objects) 
     {
         object->update(m_objects);
     }
 }
-
 void Game::draw(sf::RenderTarget& target)
 {
     // Отрисовка объектов
-    for (const auto& object : m_objects) 
+    for (GameObject* object : m_objects)
     {
         object->draw(target);
     }
