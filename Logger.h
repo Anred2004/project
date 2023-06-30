2c2149bb559604b21e2c5f3ccd1fde5d9a89855d
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-30 23:47:43 +0200

    Update Logger.h

--- Logger.h
+++ Logger.h
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-class Logger
+namespace pp 
 {
-public:
-    Logger();
-    void logError(const std::string& message);
-   
-private:
-    std::ofstream m_file;
-};
+    class Logger
+    {
+    public:
+        Logger();
+        void logError(const std::string& message);
+
+    private:
+        std::ofstream m_file;
+    };
+}
