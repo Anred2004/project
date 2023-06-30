4d31dfd96a60670128609e4c50e57b01e4a1452a
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-30 23:48:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,33 +3,41 @@
 #include <iostream>
 #include "Logger.h"
 #include "Game.h"
-
+using namespace pp;
 int main()
 {
+
     Logger logger;
     try {
         // Создание окна
-        sf::RenderWindow window(sf::VideoMode(800, 600), "Game");
+        sf::RenderWindow window(sf::VideoMode(800, 600), "Window with Texture");
+        sf::Texture backgroundTexture;
+        if (!backgroundTexture.loadFromFile("fon.png"))
+        {
+            std::cout << "error";
+        }
+        sf::Sprite backgroundSprite(backgroundTexture);
         // Создание объекта игры
         std::unique_ptr<Game> game(new Game());
         // Основной цикл программы
         while (window.isOpen()) {
             sf::Event event;
             while (window.pollEvent(event)) {
-                if (event.type == sf::Event::Closed) 
+                if (event.type == sf::Event::Closed)
                 {
                     window.close();
                 }
             }
+            window.clear();
+            window.draw(backgroundSprite);
             // Обновление игры
             game->update();
             // Отрисовка игры
-            window.clear(sf::Color::White);
             game->draw(window);
             window.display();
         }
     }
-    catch (const std::exception& ex) 
+    catch (const std::exception& ex)
     {
         // Логирование исключения
         logger.logError(ex.what());
