907328885aa541f84f5d16280f55a960e7fb8abf
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-30 23:47:23 +0200

    Update Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -1,11 +1,11 @@
 // Logger.cpp
 #include "Logger.h"
-
-Logger::Logger() 
+using namespace pp;
+Logger::Logger()
 {
     m_file.open("log.txt", std::ios_base::app);
 }
 void Logger::logError(const std::string& message)
 {
     m_file << "[ERROR] " << message << std::endl;
-} 
+}
