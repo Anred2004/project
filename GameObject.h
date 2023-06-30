40224cea71a84a974e45e38c3f9836e870767401
Anred2004 <113230298+Anred2004@users.noreply.github.com>
2023-06-30 23:46:59 +0200

    Update GameObject.h

--- GameObject.h
+++ GameObject.h
@@ -1,13 +1,12 @@
 // GameObject.h базовый класс
 #pragma once
-
 #include <SFML/Graphics.hpp>
 #include <vector>
 
 class GameObject
 {
 public:
-   
+
     virtual void update(const std::vector<GameObject*>& objects) = 0;
     virtual void draw(sf::RenderWindow& window) const = 0;//позволяет рисовать графику на отдельной области экрана или текстуры.
     virtual GameObject::~GameObject() {}
